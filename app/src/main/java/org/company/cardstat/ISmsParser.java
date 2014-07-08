b305d20f03272855c0991c6c54225e3477abf84d
vpodolyan <vpodolyan@outlook.com>
2014-07-08 16:37:34 +0400

    Добавлен комментарий

--- app/src/main/java/org/company/cardstat/ISmsParser.java
+++ app/src/main/java/org/company/cardstat/ISmsParser.java
@@ -4,6 +4,7 @@ import android.telephony.SmsMessage;
 
 import org.company.lib.BankMessage;
 
+// Интерфейс для реализации парсера SMS
 public interface ISmsParser {
     public BankMessage Parse(SmsMessage[] messages);
 }
