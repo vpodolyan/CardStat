a6d367a0124d72d27b4a4c9072e92726db1fbad1
Vladimir Podolyan <dreamcore2219@gmail.com>
2014-07-12 23:26:45 +0400

    Написал сохранение в БД распарсенного сообщения

--- app/src/main/java/org/company/cardstat/SmsMonitor.java
+++ app/src/main/java/org/company/cardstat/SmsMonitor.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.telephony.SmsMessage;
+import android.util.Log;
 
 import static android.provider.Telephony.Sms.Intents.getMessagesFromIntent;
 
@@ -11,10 +12,12 @@ import static android.provider.Telephony.Sms.Intents.getMessagesFromIntent;
  * Отслеживает SMS и отправляет их на обработку
  */
 public class SmsMonitor extends BroadcastReceiver {
+    private static final String TAG = "SmsMonitor";
     private static final String ACTION = "android.provider.Telephony.SMS_RECEIVED";
+    private static final String SAVE_ERROR_STRING = "Не удалось сохранить сообщение в БД. ";
 
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(final Context context, Intent intent) {
         if (intent != null && intent.getAction() != null && ACTION.compareToIgnoreCase(intent.getAction()) == 0) {
             final SmsMessage[] messages = getMessagesFromIntent(intent);
             if (messages == null)
@@ -26,7 +29,13 @@ public class SmsMonitor extends BroadcastReceiver {
                     BankMessage parsedMessage = parser.Parse(messages);
                     if (parsedMessage != null) {
                         // Сохранить в БД
-                        // TODO: написать сохранение в БД
+                        DatabaseHandler db = new DatabaseHandler(context);
+                        try {
+                            db.addBankMessage(parsedMessage);
+                        }
+                        catch (DatabaseHandlerException e) {
+                            Log.e(TAG, SAVE_ERROR_STRING.concat(e.getMessage()));
+                        }
                     }
                 }
             }.run();
