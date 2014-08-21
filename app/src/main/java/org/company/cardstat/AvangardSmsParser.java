76d1c0c7bb566d7bf46f019f94413b09d6365073
Vladimir Podolyan <vpodolyan@outlook.com>
2014-08-21 16:14:56 +0400

    Добавил regex в AvangardSmsParser

--- app/src/main/java/org/company/cardstat/AvangardSmsParser.java
+++ app/src/main/java/org/company/cardstat/AvangardSmsParser.java
@@ -2,14 +2,19 @@ package org.company.cardstat;
 
 import android.telephony.SmsMessage;
 
+import java.util.regex.Pattern;
+
 public class AvangardSmsParser implements ISmsParser {
     /**
      * @param message
      */
+    Pattern BUY_PATTERN = Pattern.compile("POKUPKA\\s(\\D*)\\,\\D*([0-9]+(\\.|\\,)[0-9]{1,2})\\s\\D*CARD\\*(\\d{4})");
+
+
     @Override
     public BankMessage Parse(SmsMessage message) {
-        // Найти номер карточки
         // Определить тип транзакции
+        // Найти номер карточки
         // Определить назначение
         // Определить сумму
         // Вернуть объект BankMessage
